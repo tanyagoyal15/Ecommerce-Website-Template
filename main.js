f34f71e53e31ec9729f979f61a1fd7a59dae0bfc
tanyagoyal15 <tanyagoyal15@gmail.com>
2019-07-17 14:40:02 +0530

    modified

--- main.js
+++ main.js
@@ -1,77 +1,3 @@
-/*(function ($) {
-	"use strict";
-
-
-/*---------------
-Top menu stick
------------------
-$(window).on('scroll', function() {
-	if($(this).scrollTop()>30) {
-		$('#sticky-header').addClass("sticky");
-	}
-	else {
-		$('#sticky-header').removeClass("sticky");
-	}
-});
-
-
-/*----------------------------
- TOP Menu Stick-2
------------------------------- 
-$(window).on('scroll',function() {
-if ($(this).scrollTop() > 40){  
-    $('#sticky-header-2').addClass("sticky");
-  }
-  else{
-    $('#sticky-header-2').removeClass("sticky");
-  }
-}); 
-
-/*-------------------
-tooltip
----------------------
-$('[data-toggle="tooltip"]').tooltip({
-	animated: 'fade',
-	container: 'body'
-});
-
-
-/*-------------------
- jquery meanmenu
- -------------------
- jQuery('#mobile-menu-active').meanmenu();	
-	
-/*----------------------------
- wow js active
------------------------------- 
- new WOW().init();
- 
- /*----------------------------
- nivoSlider
------------------------------- 
- $("#slider").nivoSlider({
- 	effect: 'fold',          //specify sets like :fade, fold, sliceicons
- 	slices: 15,              //for slice animations
- 	boxCols: 8,				 //for box animation
- 	boxRows: 4,              //for box animation  
- 	animSpeed: 500,           //slidetransition speed
- 	pauseTime: 3000,          //how long each slide will show
- 	startSlide: 0,            //set starting slide(0 index)
- 	directionNav: true,       //next and previous navigation
- 	controlNav: true,         //1,2,3...navigation
- 	controlNavThumbs: false,  //use thumbnails for control nav
- 	pauseOnHover : true,      //stop animaio while hovering
- 	manualAdvance : true,     //force manual transitions
- 	prevText: '<i class="fa fa-angle-left"></i>',  //prev directionNav text
- 	nextText: '<i class="fa fa-angle-right"></i>',  //next directionNav text
- 	randomStart: true,       //start on a random slide
- 	beforeChange: function(){},   //triggers before a slide transition
- 	afterChange: function(){},     //triggers after a slide transition
- 	slideshowEnd: function(){},    //triggers after all slides have been shown
- 	lastSlide: function(){},       //triggers when last slide is shown
- 	afterLoad: function(){}        //trigger when slider has ended
- });*/
-
  /*-----------------------------
  showing and hiding search bar and menu bar on clicking
  ----------------------------------*/
@@ -80,7 +6,7 @@ $(document).ready(function() {
 		$(".search-content").fadeToggle();
 		$(".close").click(function() {
 			$(".search-content").fadeToggle();
-		})
+		});
 	});
 
 	$("#show-cart").click(function() {
@@ -176,15 +102,6 @@ $('.counter').counterUp({
 	time: 1000
 });
 
-/* google map
-function myMap() {
-	var mapOptions= {
-		center: new google.maps.LatLng(51.5, -0.12),
-		zoom: 10,
-		mapTypeId: google.maps.mapTypeId.HYBRID
-	}
-	var googleMap = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-}*/
 
 ////////////////////////////////////
 //for related products
@@ -229,4 +146,3 @@ $(function() {
  	scrollSpeed: 900,
  	animation:'fade'
  });
-/*});*/
